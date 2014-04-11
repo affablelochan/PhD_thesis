65a806900348ee42dff0ab48f7fde421db33e391
rsu004 <zotov@gums.grid.latech.edu>
2014-04-10 20:13:43 -0500

    this version is compiling well on mac

--- maketex.sh
+++ maketex.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This gets the environment
-#. /etc/bashrc
+. /etc/bashrc
 
 
 
-echo maketex: run latex for file $1
+echo Markus: run latex for file $1
 
 #latex $1 && dvips -t letter -o $1.ps $1
-latex $1 && dvips  -o $1.ps $1
-ps2pdf $1.ps
-echo created files $1.ps and $1.pdf
+latex $1 && dvips -o $1.ps $1
 
+ps2pdf $1.ps
+echo created file $1.ps
